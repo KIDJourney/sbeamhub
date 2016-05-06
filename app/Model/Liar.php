59168c8e1c79e61e1b574edc1e3252433189c419
KIDJourney <kingdeadfish@qq.com>
2016-05-06 23:07:43 +0800

    add phpdoc

--- app/Model/Liar.php
+++ app/Model/Liar.php
@@ -4,6 +4,31 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Model\Liar
+ *
+ * @property integer $id
+ * @property string $tiebaid
+ * @property string $steamnick
+ * @property string $steamid
+ * @property string $taobaoid
+ * @property string $alipayaccount
+ * @property string $reason
+ * @property integer $editor
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereTiebaid($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereSteamnick($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereSteamid($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereTaobaoid($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereAlipayaccount($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereReason($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereEditor($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\Liar whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Liar extends Model
 {
     //
