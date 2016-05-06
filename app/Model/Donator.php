67b5f7351543e853267021b5574d761a9f86fad0
KIDJourney <kingdeadfish@qq.com>
2016-05-06 23:06:29 +0800

    add new model

---
+++ app/Model/Donator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Model\Donator
+ * @property string $name
+ * @property float $amount
+ * @mixin \Eloquent
+ */
+class Donator extends Model
+{
+    //
+}
