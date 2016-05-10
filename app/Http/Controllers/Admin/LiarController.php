20e86ffb5c2328e0814c9c1708c3ae7cb8bda735
KIDJourney <kingdeadfish@qq.com>
2016-05-10 22:08:54 +0800

    add show edit update function to admin liar controller

--- app/Http/Controllers/Admin/LiarController.php
+++ app/Http/Controllers/Admin/LiarController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Model\Liar;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LiarController extends Controller
 {
     //
+    public function index()
+    {
+        return Liar::all();
+    }
+
+    public function show(Liar $liar)
+    {
+        return $liar;
+    }
+
+    public function edit(Liar $liar)
+    {
+        return view('admin.liar.edit',['liar'=>$liar]);
+    }
+
+    public function update(Request $request,Liar $liar)
+    {
+        $liar->update($request->all());
+        $liar->editor = Auth::user()->id;
+        $liar->save();
+    }
+
+    
 }
