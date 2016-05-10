c18f4118610ca33716d64b202b159d8e9baa2dc8
KIDJourney <kingdeadfish@qq.com>
2016-05-10 21:02:24 +0800

    add recent scope

--- app/Model/Donator.php
+++ app/Model/Donator.php
@@ -13,4 +13,9 @@ use Illuminate\Database\Eloquent\Model;
 class Donator extends Model
 {
     //
+
+    public function scopeRecent($query, $limit=20)
+    {
+        return $query->orderBy('created_at','desc')->paginate($limit);
+    }
 }
