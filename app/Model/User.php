95750fe397152d181d243f177555cd4273f4cc65
KIDJourney <kingdeadfish@qq.com>
2016-05-10 21:00:40 +0800

    move User model

---
+++ app/Model/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * App\User
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property bool $isadmin
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Model\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function isAdmin()
+    {
+        return $this->isadmin == '1';
+    }
+}
