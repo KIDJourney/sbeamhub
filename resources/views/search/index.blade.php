f0d1fcafcd42bb325f3e4ace8a91fdd4468cd167
Rokic <Rokic.github@gmail.com>
2016-05-11 19:07:05 +0800

    modify search template

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -6,7 +6,7 @@
     <div class="col-md-6 col-md-offset-3">
         {!! Form::open(['action' => 'SearchController@show','method'=>'GET']) !!}
         <div class="input-group">
-            {!! Form::text('key', array('class'=>'form-control')) !!}
+            {!! Form::text('key', '', array('class'=>'form-control')) !!}
             <span class="input-group-btn">
                 {!! Form::submit('嗖嗖嗖', array('class'=>'btn btn-default')) !!}
             </span>
