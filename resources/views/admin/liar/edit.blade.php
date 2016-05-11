b522a1b50874183327d615245044c09192789d8d
KIDJourney <kingdeadfish@qq.com>
2016-05-11 09:37:48 +0800

    add create and edit vim

---
+++ resources/views/admin/liar/edit.blade.php
@@ -0,0 +1,1 @@
+@include('admin.liar.form_partial')
--- resources/views/admin/liar/form_partial.blade.php
+++ resources/views/admin/liar/form_partial.blade.php
@@ -1,3 +1,4 @@
+@if(isset($liar))
 {!! Form::model($liar, ['method'=>'PATCH','action'=>array('Admin\LiarController@update', $liar->id)]) !!}
     {!! Form::text('tiebaid') !!}<br>
     {!! Form::text('steamnick') !!}<br>
@@ -7,3 +8,14 @@
     {!! Form::text('reason') !!}<br>
     {!! Form::submit() !!}
 {!! Form::close() !!}
+@else
+{!! Form::open(['method'=>'POST','action'=>array('Admin\LiarController@store')]) !!}
+    {!! Form::text('tiebaid') !!}<br>
+    {!! Form::text('steamnick') !!}<br>
+    {!! Form::text('steamid') !!}<br>
+    {!! Form::text('taobaoid') !!}<br>
+    {!! Form::text('alipayaccount') !!}<br>
+    {!! Form::text('reason') !!}<br>
+    {!! Form::submit() !!}
+{!! Form::close() !!}
+@endif
