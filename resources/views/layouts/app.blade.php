4410970ad8ffd3bb4c230a8a112b2a7e147e7e2c
Rokic <Rokic.github@gmail.com>
2016-05-11 17:32:46 +0800

    modify bacic layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,6 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
                         <form class="navbar-form navbar-right in" method="POST" action="{{ url('/login') }}">
                             <div class="form-group">
                                 <input name="email" class="col-md-2 form-control" type="text" placeholder="请输入您的邮箱"/>
