2b8b2247144d0e6d9e1eb00ff3985c12c684497a
Rokic <Rokic.github@gmail.com>
2016-05-11 19:01:38 +0800

    modify search template

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,4 +1,17 @@
-{!! Form::open(['action' => 'SearchController@show','method'=>'GET']) !!}
-    {!! Form::text('key') !!}
-{!! Form::button('Submit') !!}
-{!! Form::close() !!}
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <img class="center-block" src="{{ URL::asset('image/') }}SteamHub.png">
+    <div class="col-md-6 col-md-offset-3">
+        {!! Form::open(['action' => 'SearchController@show','method'=>'GET']) !!}
+        <div class="input-group">
+            {!! Form::text('key', array('class'=>'form-control')) !!}
+            <span class="input-group-btn">
+                {!! Form::submit('嗖嗖嗖', array('class'=>'btn btn-default')) !!}
+            </span>
+        </div>
+        {!! Form::close() !!}
+    </div>
+</div>
+@endsection
