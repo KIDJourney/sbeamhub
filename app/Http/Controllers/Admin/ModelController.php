05d686373ab30564e399b9ecea315363be9f9eaf
Rokic <Rokic.github@gmail.com>
2016-05-14 06:06:10 +0800

    add ModelController

---
+++ app/Http/Controllers/Admin/ModelController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Validator;
+use App\Model;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\Admin\tabs;
+
+class ModelController extends Controller
+{
+    //TODO Not Now
+}
