ecc9bae6a4b5d616c04070197d6e63bd3d6ae6af
KIDJourney <kingdeadfish@qq.com>
2016-05-15 17:58:18 +0800

    add sales related file

---
+++ database/seeds/SaleSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class SaleSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = Faker::create();
+        $data = [];
+        foreach (range(1,100) as $index){
+            $data[] = [
+                'app_id'=>$faker->in
+            ]
+        }
+
+    }
+}
