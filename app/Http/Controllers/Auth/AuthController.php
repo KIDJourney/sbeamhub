e2ef1858033ab9051886cc15a50c5eba9fe9864d
KIDJourney <kingdeadfish@qq.com>
2016-05-16 16:16:09 +0800

    modify register method , avoid using mass assignment

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -74,10 +74,13 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        $user = new User;
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+
+        $user->save();
+
+        return $user;
     }
 }
